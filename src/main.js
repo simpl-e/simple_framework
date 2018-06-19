733e8a40d19965a4b5de9b02e35aef26d8820e23
Oscar Gardiazabal <trollderiu@gmail.com>
2018-06-19 18:34:09 -0400

    add polyfill es6

--- src/main.js
+++ src/main.js
@@ -15,16 +15,17 @@
 	var config = {
 		waitSeconds: 0, //prevent timeout by requirejs
 		paths: {
-			text: "https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min",
-			requirejs_css: "https://cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css",
-			vue: "https://cdn.rawgit.com/edgardleal/require-vuejs/aeaff6db/dist/require-vuejs" //Mismatched anonymous define() module
+			text: "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min",
+			requirejs_css: "//cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css",
+			vue: "//cdn.rawgit.com/edgardleal/require-vuejs/aeaff6db/dist/require-vuejs" //Mismatched anonymous define() module
 			,
-			jquery: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery",
-			Vue2: "https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue",
-			bootstrap: "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle",
-			axioshttp: "https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min",
-			popper: "https://unpkg.com/popper.js@1.12.6/dist/umd/popper",
-			bootstrap_notify: "https://cdnjs.cloudflare.com/ajax/libs/mouse0270-bootstrap-notify/3.1.7/bootstrap-notify.min",
+			polyfill: "//cdn.polyfill.io/v2/polyfill.min.js?features=es6",
+			jquery: "//cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery",
+			Vue2: "//cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue",
+			bootstrap: "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle",
+			axioshttp: "//cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min",
+			popper: "//unpkg.com/popper.js@1.12.6/dist/umd/popper",
+			bootstrap_notify: "//cdnjs.cloudflare.com/ajax/libs/mouse0270-bootstrap-notify/3.1.7/bootstrap-notify.min",
 		},
 		map: {
 			"*": {
@@ -68,15 +69,15 @@
 
 	var require_boot = [
 		// CSS LIBS
-		"requirejs_css!https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome",
-		"requirejs_css!https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min", //"bootstrap_notify" NEED
+		"requirejs_css!//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome",
+		"requirejs_css!//cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min", //"bootstrap_notify" NEED
 		// PRE LOAD
-		"jquery", "Vue", "axios", "notify"
+		"polyfill", "jquery", "Vue", "axios", "notify"
 	];
 
 	// NOT ESSENTIAL AND DELAYABLE LIBS
 	var require_delay = [
-		"requirejs_css!https://cdn.rawgit.com/simpl-e/simple_vue_framework/master/src/styles/bootstrap-modal-pages",
+		"requirejs_css!//cdn.rawgit.com/simpl-e/simple_vue_framework/master/src/styles/bootstrap-modal-pages",
 		"bootstrap"
 	];
 
